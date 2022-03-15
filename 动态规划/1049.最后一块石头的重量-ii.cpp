1370eff60b381392c2c6f5f2ac729fd5832d472d
1170300619 <3469205806@qq.com>
2022-03-15 21:25:02 +0800

    2022.3.15

--- 动态规划/1049.最后一块石头的重量-ii.cpp
+++ 动态规划/1049.最后一块石头的重量-ii.cpp
@@ -8,7 +8,19 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        
+        int weight = 0;
+        for(int i = 0; i < stones.size(); i++){
+            weight += stones[i];
+        }
+        int target = weight / 2;
+        vector<int> dp(target + 1, 0);
+
+        for(int i = 0; i < stones.size(); i++){
+            for(int j = target; j >= stones[i]; j--){
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return (weight - 2 * dp[target]);
     }
 };
 // @lc code=end
